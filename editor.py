b05bf6d5a12f2418ed4be92a0345bc8021d0acc0
Louis GERARD <louis.gerardd@gmail.com>
2017-11-05 15:32:31 +0100

    save code

--- editor.py
+++ editor.py
@@ -1,7 +1,10 @@
 import kivy
+import sqlite3
 
 from kivy.app import App
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 kivy.require('1.9.0')
 
@@ -9,12 +12,26 @@ kivy.require('1.9.0')
 class Editor(App):
     def __init__(self, text, **kwargs):
         super().__init__(**kwargs)
-        self.text = text
+
+        self.save_button = Button(text="Sauvegarder", size_hint=(1, .1))
+        self.save_button.bind(on_press=self.save)
+
+        self.code_input = TextInput(text=text, size_hint=(1, .9))
+        self.code_input.font_name = "code.ttf"
 
     def build(self):
-        code_input = TextInput(text=self.text)
-        code_input.font_name = "code.ttf"
-        return code_input
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(self.save_button)
+        layout.add_widget(self.code_input)
+        return layout
+
+    def save(self, value):
+        conn = sqlite3.connect('Data/kivy.db')
+        c = conn.cursor()
+        aze = (self.code_input.text,) # todo change db
+        c.execute('update IA set code=?', aze)
+        conn.commit()
+        conn.close()
 
 
 editor = Editor(text="Let's code !")
