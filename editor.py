7c7b123253074fd8fc89efbe7e864198ff6ea2d6
Louis GERARD <louis.gerardd@gmail.com>
2017-11-08 14:48:37 +0100

    Change specific IA

--- editor.py
+++ editor.py
@@ -10,8 +10,10 @@ kivy.require('1.9.0')
 
 
 class Editor(App):
-    def __init__(self, **kwargs):
+    def __init__(self, id, **kwargs):
         super().__init__(**kwargs)
+
+        self.id = id
 
         self.save_button = Button(text="Sauvegarder", size_hint=(1, .1))
         self.save_button.bind(on_press=self.save)
@@ -29,19 +31,19 @@ class Editor(App):
     def save(self, value):
         conn = sqlite3.connect('Data/kivy.db')
         c = conn.cursor()
-        aze = (self.code_input.text,) # todo change db
-        c.execute('update IA set code=?', aze)
+        aze = (self.code_input.text, self.id) # todo change db
+        c.execute('update IA set code=? where id=?', aze)
         conn.commit()
         conn.close()
 
     def load(self):
         conn = sqlite3.connect('Data/kivy.db')
         c = conn.cursor()
-        c.execute('select code from IA')
+        c.execute('select code from IA where id=?', (self.id,))
         result = c.fetchone()
         conn.close()
         return result[0]
 
 
-editor = Editor()
+editor = Editor(1)
 editor.run()
