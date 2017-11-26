0babbd8dcfd21f6f4e529f32ed80b5f67a893309
Louis GERARD <louis.gerardd@gmail.com>
2017-11-26 21:41:40 +0100

    [UNTESTED] classe IA

--- fight.py
+++ fight.py
@@ -19,58 +19,70 @@ kivy.require('1.9.0')
 Window.fullscreen = False
 
 
-# tank1 = Game.Robot(1)
-# tank2 = Game.Robot(2)
-# game = Game.Game(tank1, tank2)
-class field(App):
+tank1 = Game.Robot(1)
+tank2 = Game.Robot(2)
+game = Game.Game(tank1, tank2)
 
+
+class field(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cptTour = 0
 
         self.layout = FloatLayout()
-        self.windowWidth = Window.width
-        self.windowHeight = Window.height
+        self.caseWidth = Window.width / 32
+        self.caseHeight = Window.height / 32
 
     def build(self):
         print("build")
-        caseWidth = self.windowWidth / 32
-        caseHeight = self.windowHeight / 32
         field = InstructionGroup()
         field.add(Color(0.9294, 0.7882, 0.6863))
 
         i = 0
         while i < 32 * 32:
-            field.add(Rectangle(pos=(i % 32 * caseWidth, math.floor(i / 32) * caseHeight),
-                                size=(caseWidth - 1, caseHeight - 1)))
+            field.add(Rectangle(pos=(i % 32 * self.caseWidth, math.floor(i / 32) * self.caseHeight),
+                                size=(self.caseWidth - 1, self.caseHeight - 1)))
             i += 1
         [self.layout.canvas.add(group) for group in [field]]
         return self.layout
 
     def update(self):
         print("update")
-        caseWidth = self.windowWidth / 32
-        caseHeight = self.windowHeight / 32
 
         posTank1 = game.getPosition(1)
         posTank2 = game.getPosition(2)
         field = InstructionGroup()
         i = 0
         while i < 32 * 32:
-            if i == posTank1: #tank1
+            if i == posTank1:  # tank1
                 field.add(Color(0, 0, 1))
-            elif i == posTank2: #tank2
+            elif i == posTank2:  # tank2
                 field.add(Color(0, 1, 0))
-            else: #reste de la map
+            else:  # reste de la map
                 field.add(Color(0.9294, 0.7882, 0.6863))
-            field.add(Rectangle(pos=(i % 32 * caseWidth,
-                                        math.floor(i / 32) * caseHeight),
-                                        size=(caseWidth - 1,
-                                        caseHeight - 1)))
+            field.add(Rectangle(pos=(i % 32 * self.caseWidth,
+                                     math.floor(i / 32) * self.caseHeight),
+                                size=(self.caseWidth - 1,
+                                      self.caseHeight - 1)))
             i += 1
         game.run(self.cptTour + 1)
         [self.layout.canvas.add(group) for group in [field]]
         return self.layout
+
+
+class IA:
+    def __init__(self, id):
+        self.id = id
+
+        conn = sqlite3.connect('Data/kivy.db')
+        c = conn.cursor()
+        c.execute('SELECT code '
+                  'FROM IA '
+                  'WHERE id = ?', (id,))
+        result = c.fetchone()
+        conn.close()
+
+        self.text = result[0]
 
 
 class Tank:
@@ -80,11 +92,11 @@ class Tank:
         conn = sqlite3.connect('Data/kivy.db')
         c = conn.cursor()
         c.execute('SELECT moveValue, actionValue, range, attackcost, attackValue '
-                  'from tank t '
-                  'join caterpillar c on t.caterpillar_id = c.id '
-                  'join navSystem n on t.navSystem_id = n.id '
-                  'join weapon w on t.weapon_id = w.id '
-                  'where t.id = ?', (id, ))
+                  'FROM tank t '
+                  'JOIN caterpillar c ON t.caterpillar_id = c.id '
+                  'JOIN navSystem n ON t.navSystem_id = n.id '
+                  'JOIN weapon w ON t.weapon_id = w.id '
+                  'WHERE t.id = ?', (id,))
         result = c.fetchone()
         conn.close()
 
