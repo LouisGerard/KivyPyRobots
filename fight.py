ed60855a5ad60470d0876ef5cbe9996eff6cfa12
Louis GERARD <louis.gerardd@gmail.com>
2017-12-01 22:30:14 +0100

    fix jeu (not static anymore)

--- fight.py
+++ fight.py
@@ -17,44 +17,42 @@ import Game
 kivy.require('1.9.0')
 
 
-class Jeu(object):
-    cptTour = 0
-    caseWidth = Window.width / 32
-    caseHeight = (Window.height - 75) / 32
-    game = None
+class Jeu:
+    def __init__(self, t1, t2, ia1, ia2):
+        self.game = Game.Game(Tank(t1), Tank(t2), IA(ia1), IA(ia2))
+        self.cptTour = 0
+        self.caseWidth = Window.width / 32
+        self.caseHeight = (Window.height - 75) / 32
 
-    @staticmethod
-    def init(t1, t2, ia1, ia2):
-        Jeu.game = Game.Game(Tank(t1), Tank(t2), IA(ia1), IA(ia2))
-
-    @staticmethod
-    def incr_cpt_tour():
-        Jeu.cptTour += 1
+    def incr_cpt_tour(self):
+        self.cptTour += 1
 
 
 class Field(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.jeu = Jeu(1, 1, 1, 1)
 
     @mainthread
     def on_enter(self):
-        Jeu.init(0, 0, 1, 1)
         field = InstructionGroup()
         field.add(Color(0.9294, 0.7882, 0.6863))
 
         i = 0
         while i < 32 * 32:
-            field.add(Rectangle(pos=(i % 32 * Jeu.caseWidth, math.floor(i / 32) * Jeu.caseHeight),
-                                size=(Jeu.caseWidth - 1, Jeu.caseHeight - 1)))
+            field.add(Rectangle(pos=(i % 32 * self.jeu.caseWidth, math.floor(i / 32) * self.jeu.caseHeight),
+                                size=(self.jeu.caseWidth - 1, self.jeu.caseHeight - 1)))
             i += 1
         [self.ids.layout.canvas.add(group) for group in [field]]
         Clock.schedule_interval(self.update, 0.5)
         return self.ids.layout
 
     def update(self, dt):
-        life_tank1 = Jeu.game.getLife(0)
-        life_tank2 = Jeu.game.getLife(1)
-        pos_tank1 = Jeu.game.getPosition(0)
-        pos_tank2 = Jeu.game.getPosition(1)
-        print("tour numéro : %d" % (Jeu.cptTour,))
+        life_tank1 = self.jeu.game.getLife(0)
+        life_tank2 = self.jeu.game.getLife(1)
+        pos_tank1 = self.jeu.game.getPosition(0)
+        pos_tank2 = self.jeu.game.getPosition(1)
+        print("tour numéro : %d" % (self.jeu.cptTour,))
         print("vie tank 1 : %d" % (life_tank1,))
         print("vie tank 2 : %d \n" % (life_tank2,))
         if life_tank1 <= 0 or life_tank2 <= 0:
@@ -76,13 +74,13 @@ class Field(Screen):
                 field.add(Color(0, 1, 0))
             else:  # reste de la map
                 field.add(Color(0.9294, 0.7882, 0.6863))
-            field.add(Rectangle(pos=(i % 32 * Jeu.caseWidth,
-                                     math.floor(i / 32) * Jeu.caseHeight),
-                                size=(Jeu.caseWidth - 1,
-                                      Jeu.caseHeight - 1)))
+            field.add(Rectangle(pos=(i % 32 * self.jeu.caseWidth,
+                                     math.floor(i / 32) * self.jeu.caseHeight),
+                                size=(self.jeu.caseWidth - 1,
+                                      self.jeu.caseHeight - 1)))
             i += 1
-        Jeu.game.run(Jeu.cptTour + 1)
-        Jeu.incr_cpt_tour()
+        self.jeu.game.run(self.jeu.cptTour + 1)
+        self.jeu.incr_cpt_tour()
         [self.ids.layout.canvas.add(group) for group in [field]]
         return self.ids.layout
 
@@ -122,16 +120,16 @@ class Tank:
         conn.close()
 
         cat = namedtuple('cat', 'moveValue')
-        # todo self.caterpillar = cat(result[0])
-        self.caterpillar = cat(2)
+        self.caterpillar = cat(result[0])
+        # self.caterpillar = cat(2)
 
         nav = namedtuple('nav', 'actionValue')
-        # todo self.navSystem = nav(result[1])
-        self.navSystem = nav(2)
+        self.navSystem = nav(result[1])
+        # self.navSystem = nav(2)
 
         weap = namedtuple('weap', 'range attackCost attackValue')
-        # todo self.weapon = weap(result[2], result[3], result[4])
-        self.weapon = weap(4, 1, 5)
+        self.weapon = weap(result[2], result[3], result[4])
+        # self.weapon = weap(4, 1, 5)
 
 
 if __name__ == '__main__':
